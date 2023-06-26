3e4fdda1f07374fa664f54b8c0540d958c99ae5f
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-06-27 01:48:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
+  a = "Aryan";
   render() {
     return (
       <div>
         <Navbar/>
+        <News/>
       </div>
     )
   }
---
+++ src/components/News.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        Aaj Tak, aapke Ghar Tak
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+        <NewsItem title = "Title" description = " This is a news"/>
+      </div>
+    )
+  }
+}
+
+export default News
