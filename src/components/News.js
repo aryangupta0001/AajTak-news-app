412a38a461250e6f2889225abe7e9afc93de972b
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-06-30 02:22:37 +0530

    feat navigate to next & previous page

--- src/components/News.js
+++ src/components/News.js
@@ -13,12 +13,13 @@ export class News extends Component {
 
     this.state = {
       articles: [],
-      loading: false
-    }
+      loading: false,
+      page: 1
+    };
   }
 
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e7f3c5c6bfbd4eb09d89234855d036ba";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e7f3c5c6bfbd4eb09d89234855d036ba&page=1&pageSize=18";
     let data = await fetch(url);
     let parsedData = await data.json();
 
@@ -26,6 +27,41 @@ export class News extends Component {
 
     this.setState({ articles: parsedData.articles });
   }
+
+  prevPage = async() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e7f3c5c6bfbd4eb09d89234855d036ba&page=${this.state.page-1}&pageSize=18`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1
+    })
+  }
+
+  nextPage = async() => {
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e7f3c5c6bfbd4eb09d89234855d036ba&page=${this.state.page+1}&pageSize=18`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+  
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page + 1
+    })
+
+  }
+
+  // updateNews = async()=> {
+  //   let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e7f3c5c6bfbd4eb09d89234855d036ba&page=${this.state.page}`;
+  //   let data = await fetch(url);
+  //   let parsedData = await data.json();
+
+  //   this.setState({
+  //     articles: parsedData.articles,
+  //   })
+
+  // }
 
 
   render() {
@@ -38,14 +74,14 @@ export class News extends Component {
           </h1>
 
           {/* <img src={require(`${IMG_PATH}shweta_tiwari.jpg`)} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px" }} /> */}
-          <img src={require('./images/shweta_singh_1.jpg')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft:"25px"}} />
-          <img src={require('./images/shweta_singh_2.jpg')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft:"25px"}} />
-          <img src={require('./images/shweta_singh_3.jpg')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft:"25px"}} />
-          <img src={require('./images/shweta_singh_4.jpg')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft:"25px"}} />
-          <img src={require('./images/rl_1.jfif')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft:"25px"}} />
-          <img src={require('./images/rl_2.jfif')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft:"25px"}} />
-          <img src={require('./images/rl_3.jfif')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft:"25px"}} />
-          <img src={require('./images/rl_4.jfif')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft:"25px"}} />
+          <img src={require('./images/shweta_singh_1.jpg')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft: "25px" }} />
+          <img src={require('./images/shweta_singh_2.jpg')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft: "25px" }} />
+          <img src={require('./images/shweta_singh_3.jpg')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft: "25px" }} />
+          <img src={require('./images/shweta_singh_4.jpg')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft: "25px" }} />
+          <img src={require('./images/rl_1.jfif')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft: "25px" }} />
+          <img src={require('./images/rl_2.jfif')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft: "25px" }} />
+          <img src={require('./images/rl_3.jfif')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft: "25px" }} />
+          <img src={require('./images/rl_4.jfif')} alt="" style={{ height: "75px", width: "75px", display: "inline", borderRadius: "75px", marginLeft: "25px" }} />
         </div>
 
 
@@ -57,13 +93,24 @@ export class News extends Component {
 
               return (
                 <div className="col-md-4" key={element.url}>
-                  <NewsItem title={(element.title.length > 40 ? element.title.slice(0, 40) + "..." : element.title)} description={(element.description == null) ? (element.description) : (element.description.length > 100 ? element.description.slice(0, 100) + "..." : element.description)} imageUrl={element.urlToImage} newsUrl={element.url} />
+                  <NewsItem
+                    title={(element.title.length > 40 ? element.title.slice(0, 40) + "..." : element.title)}
+                    description={(element.description == null) ? (element.description) : (element.description.length > 100 ? element.description.slice(0, 100) + "..." : element.description)}
+                    imageUrl={element.urlToImage == null ? require("./images/default-news-image.jpg") : element.urlToImage}
+                    newsUrl={element.url}
+                  />
                 </div>
               )
             })
           }
 
         </div>
+
+        <div className="d-flex justify-content-between" style={{ margin: "50px 0px" }}>
+          <button type="button" className="btn btn-dark" onClick={this.prevPage}>&larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.nextPage}>Next &rarr;</button>
+        </div>
+
       </div>
     )
   }
