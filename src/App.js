ba78c2620bc0179fa973c16c85f1c884d69630f8
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-07-02 02:00:59 +0530

    addded router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>
-        <News pageSize = {6} country = "in" category = "general" />
-      </div>
-    )
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<News pageSize={6} country="in" category="general" />} />
+          </Routes>
+        </div>
+      </Router>
+    );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,57 +1,54 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 
 export class Navbar extends Component {
 
     render() {
         return (
-            <Router>
 
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid mx-5">
-                        <Link className="navbar-brand" to="/">AajTak</Link>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid mx-5">
+                    <Link className="navbar-brand" to="/">AajTak</Link>
 
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                                </li>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            </li>
 
-                                <li className="nav-item dropdown">
-                                    <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Country
-                                    </Link>
-                                    {/* <ul className="dropdown-menu">
+                            {/* <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Country
+                                </Link>
+                                <ul className="dropdown-menu">
                                     <li><Link className="dropdown-item" to="">Action</Link></li>
-                                    <li><Link className="dropdown-item" to="#">Another action</Link></li>
+                                    <li><Link className="dropdown-item" to="/">Another action</Link></li>
                                     <li><hr className="dropdown-divider"/></li>
-                                    <li><Link className="dropdown-item" to="#">Something else here</Link></li>
-                                </ul> */}
-                                </li>
+                                    <li><Link className="dropdown-item" to="/">Something else here</Link></li>
+                                </ul>
+                            </li> */}
 
-                                <li class="nav-item"><Link class="nav-link active" aria-current="page" to="#">General</Link></li>
-                                <li class="nav-item"><Link class="nav-link active" aria-current="page" to="#">Technology</Link></li>
-                                <li class="nav-item"><Link class="nav-link active" aria-current="page" to="#">Business</Link></li>
-                                <li class="nav-item"><Link class="nav-link active" aria-current="page" to="#">Entertainment</Link></li>
-                                <li class="nav-item"><Link class="nav-link active" aria-current="page" to="#">Sports</Link></li>
-                                <li class="nav-item"><Link class="nav-link active" aria-current="page" to="#">Health</Link></li>
-                                <li class="nav-item"><Link class="nav-link active" aria-current="page" to="#">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">General</Link></li>
+                            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Technology</Link></li>
+                            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Science</Link></li>
 
-                            </ul>
-                            <form className="d-flex" role="search">
-                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button className="btn btn-outline-success" type="submit">Search</button>
-                            </form>
-                        </div>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
                     </div>
-                </nav>
-            </Router>
-
+                </div>
+            </nav>
         )
     }
 }
