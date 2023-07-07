88b19ccf578b83be57fc856c44992502be6aa528
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-07-08 00:02:16 +0530

    removed pagination, added infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -3,6 +3,7 @@ import NewsItem from './NewsItem'
 import Loading from "./Loading";
 import PropTypes from 'prop-types'
 import InfiniteScroll from 'react-infinite-scroller';
+import jsonData from './Sample.json'
 
 import defaultNewsImage from './images/default-news-image.jpg';
 import shwetaSingh1 from './images/shweta_singh_1.jpg';
@@ -28,9 +29,14 @@ const imageMap = {
 
 export class News extends Component {
 
+  newsArticles = []
+  newsPage = 0
+  newsLoading = true
+  newsTotalArticles = 0
+
   static defaultProps = {
     country: "in",
-    pageSize: 6,
+    pageSize: 10,
     category: "general"
 
   }
@@ -44,151 +50,136 @@ export class News extends Component {
 
   }
 
-
   constructor(props) {
     super(props);
 
-    // this.prevPage = this.prevPage.bind(this);
-    // this.nextPage = this.nextPage.bind(this);
-
     document.title = `AajTak | ${this.props.title}`;
 
-
     this.state = {
-      articles: [],
+      // articles: [],
       loading: true,
-      page: 1,
-      totalArticles: 0,
+      // page: 1,
+      totalArticles: 1000,
       // api: "e7f3c5c6bfbd4eb09d89234855d036ba",           //1
       // api: "fdf7b06eac3847bbbb8733478f641676",           //2
-      // api: "72fb1894d0b7482297edd8dd614d55da",           //3
+      api: "72fb1894d0b7482297edd8dd614d55da",           //3
       // api: "b5ec941c97a5433a9b5ebe68302ce143",           //4
       // api: "f9d598beb4bf4cb78701c48fa37175ec",           //5  
       // api: "b047f87941d64bca838116aa9f5439bf"            //6
+
     };
   }
 
   async componentDidMount() {
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.state.api}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-
-    this.setState({
-      articles: parsedData.articles,
-      totalArticles: parsedData.totalResults,
-      loading: false
-    });
-  }
-
-  async prevPage() {
-    this.setState({
-      page: this.state.page - 1
-    }, () => {
-      this.updateNews();
-    });
-
-  }
-
-  async nextPage() {
-    this.setState({
-      page: this.state.page + 1
-    }, () => {
-      this.fetchMoreData();
-    })
-  }
-
-  updateNews = async () => {
+    console.log("Inside component did mount 1");
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.state.api}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-
-    this.setState({
-      loading: true,
-    })
-
     let data = await fetch(url);
     let parsedData = await data.json();
 
+    this.newsArticles = parsedData.articles;
+    this.newsTotalArticles = parsedData.totalResults;
+    this.newsLoading = false
+
     this.setState({
-      articles: parsedData.articles,
-      loading: false
+      // articles: Articles.slice((this.state.page - 1) * 6, this.state.page * 6),
+      // articles: Articles,
+      // totalArticles: parsedData.totalResults,
+      // loading: false
     })
+
+    console.log("Inside component did mount 2");
   }
+
+
+  // loadFunc = async () => {
+
+  //   this.newsPage = this.newsPage + 1
+  //   this.newsLoading = true
+  //   // this.setState({
+  //   //   page: this.state.page + 1,
+  //   //   loading: true
+  //   // });
+
+  //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.state.api}&page=${this.newsPage}&pageSize=${this.props.pageSize}`;
+
+  //   let data = await fetch(url);
+  //   let parsedData = await data.json();
+  //   let Articles = parsedData.articles;
+  //   console.log(Articles);
+
+
+  //   this.newsArticles = this.newsArticles.concat(Articles);
+  //   this.newsLoading = false;
+
+  //   this.forceUpdate();
+
+  //   // this.setState({
+  //   //   articles: this.state.articles.concat(Articles),
+  //   //   loading: false
+  //   // })
+  // }
+
 
   loadFunc = async () => {
-    this.setState({
-      page: this.state.page + 1
-    })
+    this.newsPage = this.newsPage + 1;
+    this.newsLoading = true;
 
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.state.api}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-
-    this.setState({
-      loading: true,
-    })
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.state.api}&page=${this.newsPage}&pageSize=${this.props.pageSize}`;
 
     let data = await fetch(url);
     let parsedData = await data.json();
+    let Articles = parsedData.articles;
 
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      loading: false
-    })
+    this.newsArticles = this.newsArticles.concat(Articles);
+    this.newsLoading = false;
+
+    this.forceUpdate();
   }
-
 
   render() {
     return (
-      <div className='container my-3'>
-        <div style={{ display: "flex", alignItems: "center", marginBottom: "5px" }}>
-          <h1>
-            Aaj Tak, Apke Ghar Tak
-          </h1>
+      <div className='container my-3' >
+        <h1 style={{ display: "flex", alignItems: "center", marginBottom: "5px" }}>
+          Aaj Tak, Apke Ghar Tak
+        </h1>
 
-          {Object.values(imageMap).map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt=''
-              style={{
-                height: '75px',
-                width: '75px',
-                display: 'inline',
-                borderRadius: '75px',
-                marginLeft: '25px',
-              }}
-            />
-          ))}
+        {Object.values(imageMap).map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt=''
+            style={{
+              height: '75px',
+              width: '75px',
+              display: 'inline',
+              borderRadius: '75px',
+              marginLeft: '25px',
+            }}
+          />
+        ))
+        }
 
-        </div>
-
-        {/* {this.state.loading && <Loading />} */}
-
-        {/* <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.nextPage}
-          hasMore={this.state.articles.length !== this.state.totalArticles}
-          loader={<Loading />}
-        > */}
-
-        <InfiniteScroll
+        < InfiniteScroll
           pageStart={0}
           loadMore={this.loadFunc}
-          hasMore={this.state.articles.length !== this.state.totalArticles}
-          loader={<Loading />}
-          useWindow={false}
+          hasMore={this.newsArticles.length < this.newsTotalArticles}
+          loader={< Loading key={this.newsPage} />}
+          useWindow={true}
         >
-
-
-          <div className="row container" style={{ margin: "50px auto" }}>
-            {this.state.articles.map((element, index) => {
+          <div className="row container" style={{ margin: "50px auto" }} key={this.newsPage + 1}>
+            {this.newsArticles.map((element, index) => {
 
               return (
-                <div className="col-md-4" key={index}>
-                  <NewsItem
+                <div key={index}>
+                  <NewsItem key={index + this.newsTotalArticles}
                     title={
-                      (element.title.split(" ").length > 10 ? element.title.split(" ").slice(0, 10).join(" ") + "..." : element.title)
+                      // (element.title.split(" ").length > 10 ? element.title.split(" ").slice(0, 10).join(" ") + "..." : element.title)
+                      element.title
                     }
 
                     description={
-                      (element.description === null) ? (element.description) : (element.description.split(" ").length > 25 ? element.description.split(" ").slice(0, 20).join(" ") + "..." : element.description)
+                      // (element.description === null) ? (element.description) : (element.description.split(" ").length > 25 ? element.description.split(" ").slice(0, 20).join(" ") + "..." : element.description)
+                      element.description
                     }
 
                     imageUrl={(element.urlToImage === null || element.urlToImage === "") ? defaultNewsImage : element.urlToImage}
@@ -203,27 +194,8 @@ export class News extends Component {
             })
             }
           </div>
-
         </InfiniteScroll>
-
-        {/* <div className="d-flex justify-content-between align-items-center" style={{ margin: "50px 0px" }}>
-
-        <button type="button"
-          disabled={this.state.page === 1} className="btn btn-dark"
-          onClick={this.prevPage}>
-          &larr; Previous
-        </button>
-
-        <div style={{ fontSize: "18px", fontFamily: "'Open Sans', sans-serif" }}>{this.state.page} / {Math.ceil(this.state.totalArticles / this.props.pageSize)}</div>
-        <button type="button"
-          disabled={this.state.page === Math.ceil(this.state.totalArticles / this.props.pageSize)}
-          className="btn btn-dark"
-          onClick={this.nextPage}>
-          Next &rarr;
-        </button>
-      </div> */}
-
-      </div>
+      </div >
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,27 +5,79 @@ export class NewsItem extends Component {
     render() {
         let { title, description, imageUrl, newsUrl, author, time } = this.props;
         return (
-            <div>
-                <div className="card" style={{ width: "18rem", margin: "15px auto" }}>
-                    <img src={imageUrl} className="carsd-img-top" style={{ width: "100%", height: "160px" }} alt="..." />
-                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                        {author}
-                    </span>
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
+            // <div>
+            //     <div className="card" style={{ width: "18rem", margin: "15px auto" }}>
+            //         <img src={imageUrl} className="carsd-img-top" style={{ width: "100%", height: "160px" }} alt="..." />
+            //         <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            //             {author}
+            //         </span>
+            //         <div className="card-body">
+            //             <h5 className="card-title">{title}</h5>
+            //             <p className="card-text" >{description}</p>
+            //             <p className="card-text" style={{ marginBottom: "0px" }}>
+            //                 <small className="text-body-secondary">By {author}</small>
+            //             </p>
+            //             <p className="card-text">
+            //                 <small className="text-body-secondary">{new Date(time).toGMTString()}</small>
+            //             </p>
+            //             <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-primary btn-sm">Read More</a>
+            //         </div>
+            //     </div>
+            // </div>
+            // <div className="card mb-3" style={{ maxWidth: "540px" }}>
+            //     <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            //         {author}
+            //     </span>
+            //     <div className="row g-0">
+            //         <div className="col-md-4">
+            //             <img src={imageUrl} className="img-fluid rounded-start" alt="..." />
+            //         </div>
+            //         <div className="col-md-8">
+            //             <div className="card-body">
+            //                 <h5 className="card-title">{title}</h5>
+            //                 <p className="card-text">{description}</p>
+            //                 <p className="card-text" style={{ marginBottom: "0px" }}>
+            //                     <small className="text-body-secondary">By {author}</small>
+            //                 </p>
+            //                 <p className="card-text">
+            //                     <small className="text-body-secondary">{new Date(time).toGMTString()}</small>
+            //                 </p>
+            //                 <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-primary btn-sm">Read More</a>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
 
-                        <p className="card-text" >{description}</p>
+            <div style={{ display: "flex", margin: "10px 0px", height: "30vh", border: "1px solid black", padding: "5px", justifyContent: "space-between" }}>
+                <div style={{ display: "flex", width: "20%", alignItems: "center", border: "1px solid black" }} >
+                    <img src={imageUrl} alt="" style={{ maxWidth: "100%" }} />
+                </div>
 
-                        <p className="card-text" style={{ marginBottom: "0px" }}>
-                            <small className="text-body-secondary">By {author}</small>
-                        </p>
+                <div style={{ width: "78%" }}>
+                    <h3 className="card-title">{title}</h3>
+                    <p className="card-text" style={{ fontSize: "1.15rem" }}>{description}</p>
 
-                        <p className="card-text">
-                            <small className="text-body-secondary">{new Date(time).toGMTString()}</small>
-                        </p>
+                    <div style={{ display: "flex" }}>
+                        <div>
 
-                        <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-primary btn-sm">Read More</a>
+                            <p>
+                                <small className="text-body-secondary">By {author}</small>
+
+                            </p>
+
+                            <p>
+                                <small className="text-body-secondary">{new Date(time).toGMTString()}</small>
+
+                            </p>
+                        </div>
+
+
+
+                        <div>
+
+                        </div>
                     </div>
+
                 </div>
             </div>
         )
